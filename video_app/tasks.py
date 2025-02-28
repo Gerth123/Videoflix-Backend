2ae61b8b7f47b139b0d3feed477b66f7ec8e3f0b
Gerth123 <robingerth21@gmail.com>
2025-02-28 21:56:19 +0100

    Added the converter with ffmpeg for 144p, 240p, 360p, 1080p

--- video_app/tasks.py
+++ video_app/tasks.py
@@ -10,7 +10,35 @@ import subprocess
 #     cmd = 'ffmpeg -i "{}" -s hd480 -c:v libx264 -crf 23 -c:a aac -strict -2 "{}"' .format(source, target)
 #     run = subprocess.run(cmd, capture_output=True)
 
+def convert_144p(source):
+    if source.lower().endswith('.mp4'):
+        target = source[:-4] + '.144p.mp4'
+    else:
+        target = source + '.144p.mp4'
+    source_linux = "/mnt/" + source.replace('\\', '/').replace('C:', 'c')
+    target_linux = "/mnt/" + target.replace('\\', '/').replace('C:', 'c')
+    cmd = 'ffmpeg -i "{}" -s hd144 -c:v libx264 -crf 23 -c:a aac -strict -2 "{}"' .format(source_linux, target_linux)
+    run = subprocess.run(cmd, capture_output=True)
 
+def convert_240p(source):
+    if source.lower().endswith('.mp4'):
+        target = source[:-4] + '.240p.mp4'
+    else:
+        target = source + '.240p.mp4'
+    source_linux = "/mnt/" + source.replace('\\', '/').replace('C:', 'c')
+    target_linux = "/mnt/" + target.replace('\\', '/').replace('C:', 'c')
+    cmd = 'ffmpeg -i "{}" -s hd240 -c:v libx264 -crf 23 -c:a aac -strict -2 "{}"' .format(source_linux, target_linux)
+    run = subprocess.run(cmd, capture_output=True)
+
+def convert_360p(source):
+    if source.lower().endswith('.mp4'):
+        target = source[:-4] + '.360p.mp4'
+    else:
+        target = source + '.360p.mp4'
+    source_linux = "/mnt/" + source.replace('\\', '/').replace('C:', 'c')
+    target_linux = "/mnt/" + target.replace('\\', '/').replace('C:', 'c')
+    cmd = 'ffmpeg -i "{}" -s hd360 -c:v libx264 -crf 23 -c:a aac -strict -2 "{}"' .format(source_linux, target_linux)
+    run = subprocess.run(cmd, capture_output=True)
 
 #linux:
 def convert_480p(source):
@@ -22,3 +50,24 @@ def convert_480p(source):
     target_linux = "/mnt/" + target.replace('\\', '/').replace('C:', 'c')
     cmd = 'ffmpeg -i "{}" -s hd480 -c:v libx264 -crf 23 -c:a aac -strict -2 "{}"' .format(source_linux, target_linux)
     run = subprocess.run(cmd, capture_output=True, shell=True)
+
+def convert_720p(source):
+    if source.lower().endswith('.mp4'):
+        target = source[:-4] + '.720p.mp4'
+    else:
+        target = source + '.720p.mp4'
+    source_linux = "/mnt/" + source.replace('\\', '/').replace('C:', 'c')
+    target_linux = "/mnt/" + target.replace('\\', '/').replace('C:', 'c')
+    cmd = 'ffmpeg -i "{}" -s hd720 -c:v libx264 -crf 23 -c:a aac -strict -2 "{}"' .format(source_linux, target_linux)
+    run = subprocess.run(cmd, capture_output=True, shell=True)
+
+def convert_1080p(source):
+    if source.lower().endswith('.mp4'):
+        target = source[:-4] + '.1080p.mp4'
+    else:
+        target = source + '.1080p.mp4'
+    source_linux = "/mnt/" + source.replace('\\', '/').replace('C:', 'c')
+    target_linux = "/mnt/" + target.replace('\\', '/').replace('C:', 'c')
+    cmd = 'ffmpeg -i "{}" -s hd1080 -c:v libx264 -crf 23 -c:a aac -strict -2 "{}"' .format(source_linux, target_linux)
+    run = subprocess.run(cmd, capture_output=True, shell=True)
+
