17c8c2c8eb40a0b0783349caee29adb5203e39f1
Gerth123 <robingerth21@gmail.com>
2025-04-14 22:45:01 +0200

    Changed import at settings.

--- videoflix_backend_hub/settings.py
+++ videoflix_backend_hub/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 import os
-from static.passwords import *
+from static.passwords import GMAIL_PASSWORD
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
