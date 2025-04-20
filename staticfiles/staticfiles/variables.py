05d68725027dafea1ae660c1a37cd2c920d06189
Gerth123 <robingerth21@gmail.com>
2025-04-20 18:40:00 +0200

    Added the assets to git and the server.

--- staticfiles/staticfiles/variables.py
+++
@@ -1,5 +0,0 @@
-EMAIL_HOST_USER = "robingerth21@gmail.com"
-EMAIL_HOST_PASSWORD = "rdcammpycsdrhlrb"
-DB_PASSWORD = "neues_passwort123"
-SECRET_KEY = "django-insecure-fyquy_r9b_pq-^dd7c(5c32**n@3dyj-h*nqn(#9hulh&#maz0"
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'testserver', '34.65.107.197', 'videoflix.robin-gerth.de']
